607918450fd7eae7b36a92e943a9867dc2d528e0
PiotrKorczyk <p.k.6@wp.pl>
2017-05-03 16:52:19 +0200

    Basics works correct

--- Draw.java
+++ Draw.java
@@ -1,30 +1,51 @@
 package java_lab_04;
 
 import javax.swing.JComponent;
-import java.awt.Graphics;
+import java.awt.*;
 
 public class Draw extends JComponent {
 
-    private int[] xP;
-    private int[] yP;
+    private int [] xP = new int [3];
+    private int [] yP = new int [3];
+    
     private int numberOfVertices;
+
+//    public void setNumberOfVertices(int numberOfVertices) {
+//        this.numberOfVertices = numberOfVertices;
+//    }
+//
+//    public void setxP(int[] xP) {
+//        this.xP = xP;
+//    }
+//
+//    public void setyP(int[] yP) {
+//        this.yP = yP;
+//    }
+    
 
     public void setXPAndYP(int[] _xP, int[] _yP, int _numberOfVertices) {
         this.xP = new int[_numberOfVertices];
-        this.yP = new int[_numberOfVertices];
+         this.yP = new int[_numberOfVertices];
         this.numberOfVertices = _numberOfVertices;
         for (int i = 0; i <= _numberOfVertices - 1; i++) {
-            xP[i] = _xP[i];
-            yP[i] = _yP[i];
+            this.xP[i] = _xP[i];
+            this.yP[i] = _yP[i];
             System.out.println(xP[i] + "   "+yP[i]);
         }
+        System.out.println(numberOfVertices);
     }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for(int i=0;i<=numberOfVertices-1;i++){
+            System.out.println(xP[i]+"  "+yP[i]);
+        }
+        Polygon poly = new Polygon(xP, yP, numberOfVertices);
+            g2.drawPolygon(poly);
+//        g2.drawPolygon(xP, yP, numberOfVertices);
 //        g.drawRect(150,150,200,200);
-        g.drawPolygon(xP, yP, numberOfVertices);
+//        g.drawPolygon(xP, yP, numberOfVertices);
     }
 }
     
--- Main.java
+++ Main.java
@@ -1,16 +1,31 @@
 package java_lab_04;
-import java.awt.Graphics;
 
-class Main{
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
 
-   
-public static void main(String [] args){
-    int [] x ={100,200,400};
-    int [] y ={300,300,500}; 
-    SetWindow win = new SetWindow();
-    Draw draw = new Draw();
-  
-    draw.setXPAndYP( x, y, 3);
-    draw.paint(Graphics g);
-}
+class Main {
+
+    public static void main(String[] args) {
+
+        JFrame myWindow = new JFrame();
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        myWindow.setSize(screenSize.width / 2, screenSize.height / 2);
+        myWindow.setResizable(false);
+        myWindow.getContentPane().add(new Draw());
+        myWindow.setVisible(true);
+        Draw draw = new Draw();
+        int[] x = {100, 200, 400};
+        int[] y = {300, 300, 200};
+        
+//        draw.setxP(x);
+//        draw.setyP(y);
+//        draw.setNumberOfVertices(3);
+
+        draw.setXPAndYP(x, y, 3);
+
+        myWindow.add(draw);
+
+    }
 }
