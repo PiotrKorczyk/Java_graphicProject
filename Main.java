8d39b1f433b0d8939d710359809ed206e11af2a7
PiotrKorczyk <p.k.6@wp.pl>
2017-05-05 13:19:52 +0200

    add EventQueue and window position

--- Main.java
+++ Main.java
@@ -8,24 +8,28 @@ class Main {
 
     public static void main(String[] args) {
 
-        JFrame myWindow = new JFrame();
-        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        myWindow.setSize(screenSize.width / 2, screenSize.height / 2);
-        myWindow.setResizable(false);
-        myWindow.getContentPane().add(new Draw());
-        myWindow.setVisible(true);
-        Draw draw = new Draw();
         int[] x = {100, 200, 400};
         int[] y = {300, 300, 200};
-        
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame myWindow = new JFrame();
+                myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                myWindow.setSize(dim.width / 2, dim.height / 2);
+                myWindow.setLocation((dim.width/2)/2, (dim.height/2)/2);
+                myWindow.setResizable(false);
+                myWindow.getContentPane().add(new Draw());
+                myWindow.setVisible(true);
+                Draw draw = new Draw();
+                myWindow.add(draw);
+                draw.setXPAndYP(x, y, 3);
+
+            }
+        });
+
 //        draw.setxP(x);
 //        draw.setyP(y);
 //        draw.setNumberOfVertices(3);
-
-        draw.setXPAndYP(x, y, 3);
-
-        myWindow.add(draw);
-
     }
 }
